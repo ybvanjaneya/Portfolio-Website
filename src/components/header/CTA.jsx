014ba58738746525c07998c05c58e67201ea1207
ybvanjaneya <127592510+ybvanjaneya@users.noreply.github.com>
2023-07-07 00:16:15 +0530

    Added project base folders

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CV from '../../assets/Y B V Anjaneya Resume.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={CV} download className='btn'>Download CV</a>
+    </div>
+  )
+}
+
+export default CTA
